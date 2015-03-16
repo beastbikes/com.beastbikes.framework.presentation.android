a43ea9422e5e99bb3f16e39cc0f433812586f374
johnsonlee <g.johnsonlee@gmail.com>
2015-03-16 16:30:13 +0800

    Fix issue of displaying image from local file

--- main/src/com/beastbikes/framework/ui/android/widget/AsyncImageView.java
+++ main/src/com/beastbikes/framework/ui/android/widget/AsyncImageView.java
@@ -1,9 +1,14 @@
 package com.beastbikes.framework.ui.android.widget;
 
+import java.io.File;
+
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.beastbikes.framework.android.cache.CacheManager;
 
 /**
  * The asynchronously loaded image view
@@ -12,6 +17,8 @@ import com.android.volley.toolbox.NetworkImageView;
  * 
  */
 public class AsyncImageView extends NetworkImageView {
+
+	private Bitmap local;
 
 	public AsyncImageView(Context context) {
 		this(context, null);
@@ -23,6 +30,36 @@ public class AsyncImageView extends NetworkImageView {
 
 	public AsyncImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+	}
+
+	public void setImageBitmap(File file) {
+		this.setImageBitmap(file.getAbsolutePath());
+	}
+
+	public void setImageBitmap(String path) {
+		final CacheManager cm = CacheManager.getInstance();
+		this.local = cm.getBitmap(path);
+		if (null == this.local) {
+			this.local = cm.loadBitmapFromFile(path);
+		}
+
+		this.requestLayout();
+	}
+
+	@Override
+	public void setImageUrl(String url, ImageLoader imageLoader) {
+		this.local = null;
+		super.setImageUrl(url, imageLoader);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
+
+		if (null != this.local) {
+			super.setImageBitmap(this.local);
+		}
 	}
 
 }
